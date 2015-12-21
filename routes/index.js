35c81cf0cafaab1411b3909043e9912a30a56a1d
Adam Weitzman <adamcweitzman@gmail.com>
2015-12-21 13:15:48 -0600

    can get object on client side

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,13 @@ var medical_data = require('../model/Medical_Data')
 
 
 router.get('/', function(req, res, next) {
-	// console.log(medical_data, "medical_data is...")
- //    medical_data.find({'State':'AZ'}, function(err, data) {
-    // });
 	res.render('index');
  });
 
 router.get('/data', function(req, res, next) {
-	client_data = [4, 8, 15, 16, 23, 42]
-	res.json(client_data);
+    medical_data.findOne({'State':'AZ'}, function(err, data) {
+    	res.json(data);
+    });
  });
 
 module.exports = router;
