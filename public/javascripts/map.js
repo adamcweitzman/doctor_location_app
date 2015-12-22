bd9426f0e754c9b4ad039406f67d8c98f536c8a6
Adam Weitzman <adamcweitzman@gmail.com>
2015-12-22 10:57:23 -0600

    and now im using mapbox and im :)

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,24 +1,53 @@
 $(function() {
 
-	var canvas = d3.select('#map').append("svg")
-		.attr("width", 300)
-		.attr("height", 200)
+L.mapbox.accessToken = 'pk.eyJ1IjoiYWRhbWN3ZWl0em1hbiIsImEiOiJjaWlobWd2dXcwMGFkdHBrcDZjN3pyNWF5In0.b59dgwDArrXvBD_1JJDI1g';
+var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([0, 0], 2);
 
-	d3.json("us.geojson", function (data) {
-		var group = canvas.selectAll("g")
-		.data(data.features)
-		.enter()
-		.append("g")
+var coordinates = document.getElementById('coordinates');
 
-	var projection = d3.geo.mercator().scale(200);
-	var path = d3.geo.path().projection(projection);
+var marker = L.marker([0, 0], {
+    icon: L.mapbox.marker.icon({
+      'marker-color': '#f86767'
+    }),
+    draggable: true
+}).addTo(map);
 
-	var areas = group.append("path")
-		.attr("d", path)
-		.attr("class", "area")
-		.attr("fill", "steelblue ")
-	});	
+// every time the marker is dragged, update the coordinates container
+marker.on('dragend', ondragend);
+
+// Set the initial marker coordinate on load.
+ondragend();
+
+function ondragend() {
+    var m = marker.getLatLng();
+    coordinates.innerHTML = 'Latitude: ' + m.lat + '<br />Longitude: ' + m.lng;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
 
 
 
