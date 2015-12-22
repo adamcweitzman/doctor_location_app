7781c5bd3b18c3382c89d7a192e6bf258493f0e6
Adam Weitzman <adamcweitzman@gmail.com>
2015-12-22 14:59:45 -0600

    a little styling

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -6,7 +6,14 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([40, -99]).addTo(map)
+var circle = L.circle([40, -99], 500, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
+
+//google geocdoe API KEY
+//AIzaSyCfX95ESW8DqYM6CewKAD20ArPD4ZN0Jdc 
 
 
 
