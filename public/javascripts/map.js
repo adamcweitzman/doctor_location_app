79c60460376570dce200d8c624cab8e0c8e8a885
Adam Weitzman <adamcweitzman@gmail.com>
2015-12-21 18:50:29 -0600

    there's a map, but its missing california and tennesee?

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,3 +1,38 @@
 $(function() {
 
+	var canvas = d3.select("body").append("svg")
+		.attr("width", 760)
+		.attr("height", 700)
+
+	d3.json("us.geojson", function (data) {
+		var group = canvas.selectAll("g")
+		.data(data.features)
+		.enter()
+		.append("g")
+
+	var projection = d3.geo.mercator();
+	var path = d3.geo.path().projection(projection);
+
+	var areas = group.append("path")
+		.attr("d", path)
+		.attr("class", "area")
+		.attr("fill", "steelblue ")
+	});	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
