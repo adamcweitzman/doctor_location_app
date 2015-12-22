4ffdbbadec2146651931b11656474913eae0f6ad
Adam Weitzman <adamcweitzman@gmail.com>
2015-12-22 11:57:36 -0600

    have the map centered at us

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,28 +1,38 @@
 $(function() {
 
-L.mapbox.accessToken = 'pk.eyJ1IjoiYWRhbWN3ZWl0em1hbiIsImEiOiJjaWlobWd2dXcwMGFkdHBrcDZjN3pyNWF5In0.b59dgwDArrXvBD_1JJDI1g';
-var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([0, 0], 2);
+// L.mapbox.accessToken = 'pk.eyJ1IjoiYWRhbWN3ZWl0em1hbiIsImEiOiJjaWlobWd2dXcwMGFkdHBrcDZjN3pyNWF5In0.b59dgwDArrXvBD_1JJDI1g';
+// var map = L.mapbox.map('map', 'mapbox.streets')
+//     .setView([0, 0], 2);
 
-var coordinates = document.getElementById('coordinates');
+// var coordinates = document.getElementById('coordinates');
 
-var marker = L.marker([0, 0], {
-    icon: L.mapbox.marker.icon({
-      'marker-color': '#f86767'
-    }),
-    draggable: true
+// var marker = L.marker([0, 0], {
+//     icon: L.mapbox.marker.icon({
+//       'marker-color': '#f86767'
+//     }),
+//     draggable: true
+// }).addTo(map);
+
+// // every time the marker is dragged, update the coordinates container
+// marker.on('dragend', ondragend);
+
+// // Set the initial marker coordinate on load.
+// ondragend();
+
+// function ondragend() {
+//     var m = marker.getLatLng();
+//     coordinates.innerHTML = 'Latitude: ' + m.lat + '<br />Longitude: ' + m.lng;
+// }
+var map = L.map('map').setView([40, -99], 4);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// every time the marker is dragged, update the coordinates container
-marker.on('dragend', ondragend);
+L.marker([40, -99]).addTo(map)
 
-// Set the initial marker coordinate on load.
-ondragend();
 
-function ondragend() {
-    var m = marker.getLatLng();
-    coordinates.innerHTML = 'Latitude: ' + m.lat + '<br />Longitude: ' + m.lng;
-}
+
 
 
 
