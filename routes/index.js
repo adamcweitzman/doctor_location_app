eacfe4606d69aa053e06f3b774108b528a17e69b
Adam Weitzman <adamcweitzman@gmail.com>
2015-12-30 16:56:52 -0600

    can get object with unique elements and the amount of times that they occur

--- routes/index.js
+++ routes/index.js
@@ -38,9 +38,48 @@ router.post('/upload', function(req, res, next) {
 	q = zips.find({ 'zip':{$in:[zipcode,plus1,plus2,plus3,plus4,plus5,minus1,minus2,minus3,minus4,minus5]}});
 	// q = zips.find({'zip':zipcode}).limit(10)
     q.exec(function(err, data) {
-    //loop over array of objects and create new array of unique zips and their numbers
-    	console.log(data)
-    	res.send(data)
+    //loop over array of objects and create new array of objects of unique zips and number of their occurences
+
+    function toArray (array) {
+	    distinct = []
+	    for (var i = 0; i < array.length; i++) {
+	            if ((array[i].zip in distinct) == false) {
+	                distinct.push(array[i].zip)
+	             }
+	    }
+	    return distinct
+	}
+
+	function foo(arr) {
+	    var a = [], b = [], prev;
+
+	    arr.sort();
+	    for ( var i = 0; i < arr.length; i++ ) {
+	        if ( arr[i] !== prev ) {
+	            a.push(arr[i]);
+	            b.push(1);
+	        } else {
+	            b[b.length-1]++;
+	        }
+	        prev = arr[i];
+	    }
+	    return [a, b];
+	}
+
+	function combine(names, values) {
+	    var result = {};
+	    for (var i = 0; i < names.length; i++)
+	         result[names[i]] = values[i];
+	    return result;
+	}
+
+	var unique = toArray(data) // returns ['a', 1, 2, '1']
+	unique = foo(unique)
+	unique = combine(unique[0],unique[1])
+
+	console.log(unique)
+	res.send(unique)
+
     });
 });
 
